80dba486bf34edce9076f83942ec0ef890161055
rprakas2 <rprakas@ncsu.edu>
2024-05-19 13:15:38 +0530

    Edited from Jupyter

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,6 @@ def greet(name):
     return f"Hello, {name}!"
 
 if __name__=="__main__":
-    user_name =input("Enter you name: ")
+    user_name =input("Enter you name: ") #Enter your name
     print(greet(user_name))
     
